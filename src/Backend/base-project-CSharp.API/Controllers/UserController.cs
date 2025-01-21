7f70835f9eadaf034753d91b8dfabef6eacf04fe
Matheus Nery <matheusnery345@outlook.com>
2025-01-21 14:49:33 -0400

    feat: adding endpoint to register user

---
+++ src/Backend/base-project-CSharp.API/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using base_project_CSharp.Communication.Requests;
+using base_project_CSharp.Communication.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace base_project_CSharp.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisterUserJson), StatusCodes.Status201Created)]
+        public IActionResult RegisterUser(RequestRegisterUserJson request)
+        {
+            return null;
+        }
+    }
+}
