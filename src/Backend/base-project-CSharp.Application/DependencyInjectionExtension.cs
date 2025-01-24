62bf2cdd759c9cf24b76ff921fd9d9e46ff56246
Matheus Nery <matheusnery345@outlook.com>
2025-01-24 11:40:54 -0400

    feat: Adding Dependency Injection

---
+++ src/Backend/base-project-CSharp.Application/DependencyInjectionExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace base_project_CSharp.Application
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+
+        }
+    }
+}
---
+++ src/Backend/base-project-CSharp.Infrastructure/DependencyInjectionExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace base_project_CSharp.Infrastructure
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddInfrasctructure(this IServiceCollection services)
+        {
+
+        }
+    }
+}
