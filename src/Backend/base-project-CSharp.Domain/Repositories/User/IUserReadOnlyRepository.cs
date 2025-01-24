11040375301dee35d3be58dbeb8067c64bc27c4f
Matheus Nery <matheusnery345@outlook.com>
2025-01-24 11:13:33 -0400

    feat: Adding repository methods to user

---
+++ src/Backend/base-project-CSharp.Domain/Repositories/User/IUserReadOnlyRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace base_project_CSharp.Domain.Repositories.User
+{
+    public interface IUserReadOnlyRepository
+    {
+        public Task<bool> ExistActiveUserWithEmail(string email);
+    }
+}
