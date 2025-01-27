83a2c957d8f8b5c3cbcecf463d998ff590a603eb
Matheus Nery <matheusnery345@outlook.com>
2025-01-27 11:44:54 -0400

    feat: Including addictional key from password on .env

--- src/Backend/base-project-CSharp.Application/DependencyInjectionExtension.cs
+++ src/Backend/base-project-CSharp.Application/DependencyInjectionExtension.cs
@@ -1,16 +1,17 @@
 ﻿using base_project_CSharp.Application.Cryptography;
 using base_project_CSharp.Application.Services.AutoMapper;
 using base_project_CSharp.Application.UseCases.User.Register;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace base_project_CSharp.Application
 {
     public static class DependencyInjectionExtension
     {
-        public static void AddApplication(this IServiceCollection services)
+        public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
         {
             AddAutoMapping(services);
-            AddEncripter(services);
+            AddEncripter(services, configuration);
             AddUseCases(services);
         }
 
@@ -22,9 +23,10 @@ namespace base_project_CSharp.Application
             }).CreateMapper());
         }
 
-        private static void AddEncripter(IServiceCollection services)
+        private static void AddEncripter(IServiceCollection services, IConfiguration configuration)
         {
-            services.AddScoped(options => new PasswordEncripter());
+            var addictionalKey = configuration.GetValue<string>("Settings:Password:AddictionalKey");
+            services.AddScoped(options => new PasswordEncripter(addictionalKey!));
         }
 
         private static void AddUseCases(IServiceCollection services)
